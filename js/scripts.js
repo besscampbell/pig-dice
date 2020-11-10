2f8cd63bfe832936d46dbbe6cefe134e94aa6a25
Bess Campbell <bess.k.campbell@gmail.com>
2020-11-09 22:02:11 -0800

    Add display scores and add an alert when a 1 is rolled.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // Business Logic
 let users = new Users();
-let player1 = new Player("player1", true, [], 0);
-let player2 = new Player("player2", false, [], 0);
+let player1 = new Player("Player One", true, [], 0);
+let player2 = new Player("Player Two", false, [], 0);
 users.players.push(player1, player2);
 
 
@@ -23,8 +23,7 @@ Player.prototype.diceRoll = function(){
       player1.turn= !player1.turn
       player2.turn= !player2.turn
       this.roundScore = [];
-      console.log(this);
-      return this.roundScore;
+      alert("You rolled in the mud with that ONE! Your turn is over!");
   } else if (roll !=1) {
       this.roundScore.push(roll);
       return this.roundScore;
@@ -35,65 +34,49 @@ Player.prototype.hold = function() {
   if (this.roundScore[0] != 0) {
     const add = (a, b) => a+b;
     this.totalScore = this.totalScore + this.roundScore.reduce(add);
-    console.log(this.totalScore);
     this.roundScore = [];
     player1.turn= !player1.turn
     player2.turn= !player2.turn
-    console.log(this);
-    return this.roundScore;
-    if (this.totalScore >= 100) {
-      alert("Back to the sty! You pass the pigs the best! You win!");
-    }
+    return this.roundScore; 
   }
-};
+  if (this.totalScore >= 100) {
+      alert("Back to the sty! You pass the pigs the best! You win!");
+  }
+ };
 
 Users.prototype.whoseTurn = function() {
   for (let i=0; i< this.players.length; i++){
-    // if (this.players[i]){
-      if (this.players[i].turn === true) {
-      return this.players[i].diceRoll();
-      }
-    // }  
+    if (this.players[i].turn === true) {
+    return this.players[i].diceRoll();
+    }
   }
 }
 
 Users.prototype.holdTurn = function() {
   for (let i=0; i< this.players.length; i++){
-    // if (this.players[i]){
-      if (this.players[i].turn === true) {
-        console.log(this.players[i]);
-        return this.players[i].hold();
-      }
-    // }  
+    if (this.players[i].turn === true) {
+      return this.players[i].hold();
+    } 
   }
 }
 
-// Logic UI
-
-// Player.prototype.dislplayUsersScores = function() {
-//   let player1Score = $("#playerOneArray");
-//   let player2Score = $("#playerTwoArray");
-//   player1Score.text(player1.roundScore);
-//   player2Score.text(player2.roundScore);
-//   }
-
 $(document).ready(function() {
-
-
+  let playerOne = player1.user;
+  $("#playerOneName").text(playerOne);
+  let playerTwo = player2.user;
+  $("#playerTwoName").text(playerTwo);
   $(".img").click(function(){
-    // player1.displayUsersScores();
-    let player1Score = player1.roundScore
-    $("#playerOneArray").text(player1Score + " ")
-    // $("#playerOneArray").html(player1.diceRoll());
     users.whoseTurn();
-    });
-  // });  
+    let player1Score = player1.roundScore
+    $("#playerOneArray").text(player1Score)
+    let player2Score = player2.roundScore
+    $("#playerTwoArray").text(player2Score)
+    let player1Total = player1.totalScore;
+    $("#playerOneScore").text(player1Total);
+    let player2Total = player2.totalScore;
+    $("#playerTwoScore").text(player2Total);
+  });  
   $("#hold").click(function(){
-    
     users.holdTurn();
   });
-  // $(".img").click(function() { document.getElementById("#playerOneArray").innerHTML = player1.roundScore})
-  // let player1Score = player1.roundScore
-  // $(".img").click(function(){$("#playerOneArray").text(player1Score)})
-  // console.log(player1.roundScore)
 });
